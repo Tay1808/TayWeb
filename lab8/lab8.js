fc8ac1fc69953460f8ffe64d4469cc5f6ca06609
Таня Лоринец <Lorinecz@yandex.ru>
2024-05-31 18:31:45 +0700

    изменения в выводе даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -3,21 +3,21 @@ function showDate() {
     let today = new Date();
     let russianDate = today.toLocaleString('ru-RU');
     let spain = new Date();
-    spain.setHours(7);
+    spain.setHours(14);
     let spanishDate = spain.toLocaleString('es-ES');
     let german = new Date();
-    german.setHours(7);
+    german.setHours(14);
     let germanDate = german.toLocaleString('de-DE');
     let france = new Date();
-    france.setHours(7);
+    france.setHours(14);
     let franceDate = france.toLocaleString('fr-FR');
-    let polish = new Date();
-    polish.setHours(7);
-    let polishDate = polish.toLocaleString('pl-PL');
+    let egypt = new Date();
+    let egyptDate = egypt.toLocaleString('ar-EG'); 
     
     out.innerHTML = "Дата и время для русской локали : " + russianDate + "<br>";
     out.innerHTML += "Дата и время для испанской локали : " + spanishDate + "<br>"; 
+    out.innerHTML += "Дата и время для локали Египта: " + egyptDate + "<br>";   
     out.innerHTML += "Дата и время для локали Германии : " + germanDate + "<br>"; 
-    out.innerHTML += "Дата и время для локали Франции: " + franceDate + "<br>";
-    out.innerHTML += "Дата и время для локали Польши: " + polishDate;  
+    out.innerHTML += "Дата и время для локали Франции: " + franceDate;
+
 }
