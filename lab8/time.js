348e415fe1811a9c05dd492b2537382f37e4bd05
Таня Лоринец <Lorinecz@yandex.ru>
2024-06-02 16:46:50 +0700

    аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -6,3 +6,17 @@ function showTime() {
     document.getElementById('seconds').innerHTML = sec;
 }
 setInterval(showTime, 1000);
+
+
+let totalDegrees = 0;
+
+function showhand() {
+    let secondHand = document.getElementById('second-hand');
+    let now = new Date();
+    let seconds = now.getSeconds();
+    totalDegrees = seconds * 6;  // 360 degrees in a full circle, moving 6 degrees every second
+    secondHand.style.transform = `rotate(${totalDegrees}deg)`;
+}
+
+setInterval(showhand, 1000);
+
